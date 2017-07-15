9bf5fa61d54c15d500662b26f24a3f8c4efb20f1
Thierry Danis <tdanis@cisco.com>
2017-07-15 13:06:50 +0200

    Add an option to specify a delay before really starting

--- DetectCaptivePortal.js
+++ DetectCaptivePortal.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
 
 var exec = require("child_process").exec;
-var pvddCnx = require("pvdd").connect({ autoReconnect : true });
+var pvdd = require("pvdd");
 
+var pvddCnx = null;
 var allPvd = {};
 
-pvddCnx.on("connect", function() {
+function handleConnection() {
 	allPvd = {};
         pvddCnx.getList();
         pvddCnx.subscribeNotifications();
         pvddCnx.subscribeAttribute("*");
         console.log("Connection established with pvdd");
-});
-pvddCnx.on("error", function(err) {
-        console.log("Connection lost with pvdd (" + err.message + ")");
-});
+}
 
-pvddCnx.on("pvdList", function(pvdList) {
+function handleError(err) {
+        console.log("Connection lost with pvdd (" + err.message + ")");
+}
+
+function handlePvdList(pvdList) {
 	pvdList.forEach(function(pvdname) {
 		if (allPvd[pvdname] == null) {
 			allPvd[pvdname] = { captivePortal : null };
 			pvddCnx.getAttributes(pvdname);
 		}
 	});
-});
+}
 
-pvddCnx.on("delPvd", function(pvdname) { allPvd[pvdname] = null; });
+function handleDelPvd(pvdname) {
+	allPvd[pvdname] = null;
+}
 
-pvddCnx.on("pvdAttributes", function(pvdname, attrs) {
+function handlePvdAttributes(pvdname, attrs) {
 	console.log("Attributes for " + pvdname + " = " +
 			JSON.stringify(attrs, null, "\t"));
 
@@ -42,4 +46,50 @@ pvddCnx.on("pvdAttributes", function(pvdname, attrs) {
 		console.log("Starting firefox on " + captivePortal);
 	}
 	allPvd[pvdname].captivePortal = captivePortal;
+}
+
+function Monitor() {
+	pvddCnx = pvdd.connect({ autoReconnect : true });
+
+	pvddCnx.on("connect", handleConnection);
+
+	pvddCnx.on("error", handleError);
+
+	pvddCnx.on("pvdList", handlePvdList);
+
+	pvddCnx.on("delPvd", handleDelPvd);
+
+	pvddCnx.on("pvdAttributes", handlePvdAttributes);
+}
+
+var Help = false;
+var Verbose = false;
+var DelayExpected = false;
+var Delay = null;
+
+process.argv.forEach(function(arg) {
+	if (arg == "-h" || arg == "--help") {
+		Help = true;
+	} else
+	if (arg == "-v" || arg == "--verbose") {
+		Verbose = true;
+	} else
+	if (arg == "-d" || arg == "--delay") {
+		DelayExpected = true;
+	} else
+	if (DelayExpected) {
+		DelayExpected = false;
+		Delay = parseInt(arg);
+	}
 });
+
+
+if (Delay != null) {
+	setTimeout(Monitor, Delay * 1000);
+}
+else {
+	Monitor();
+}
+
+
+/* ex: set ts=8 noexpandtab wrap: */
